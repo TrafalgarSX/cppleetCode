6b9f95385bb47f26ea6d73ace1d2f5d9f901573d
Trafalgar <1148636359g@gmail.com>
2023-05-18 22:49:40 +0800

    5.18 双指针按情况轮流往中间移动，最后一定是在first固定的情况下最接近target的值。 最后在所有的first 对应的 最小sum中选出一个最接近target的。

--- 16.最接近的三数之和.cpp
+++ 16.最接近的三数之和.cpp
@@ -6,28 +6,46 @@
 
 // @lc code=start
 #include <vector>
+#include<cmath>
+#include<algorithm>
 using namespace std;
 
 class Solution {
 public:
   int threeSumClosest(vector<int> &nums, int target) {
-    int ret;
+    int ret = 1e7;
     int first, second, third, tmp_target;
     int size = nums.size();
-    int min;
+    sort(nums.begin(), nums.end());
 
     for (first = 0; first < size; ++first) {
       if (first > 0 && nums[first] == nums[first - 1])
         continue;
 
-      third = size - 1;
-      for (second = first + 1; second < size; ++second) {
-        if (second > first + 1 && nums[second] == nums[second - 1])
-          continue;
+      second = first + 1, third = size - 1;
+      while(second < third){
+        int sum = nums[first] + nums[second] + nums[third];
+        if(sum == target){
+            return target;
+        }
 
-        while(second < third && abs(nums[first] + nums[second] + nums[third] - target) < min)
-            third--;
+        if(abs(sum - target) < abs(ret - target)){
+            ret = sum;
+        }
 
+        if(sum > target){
+            int temp = third - 1;
+            while(second < temp && nums[temp] == nums[third]){
+                temp--;
+            }
+            third = temp;
+        }else {
+            int temp = second + 1;
+            while(temp < third && nums[temp] == nums[second]){
+                temp++;
+            }
+            second = temp;
+        }
       }
     }
 
@@ -35,3 +53,13 @@ public:
   }
 };
 // @lc code=end
+
+
+// 应该是 lamada表达式
+        // // 根据差值的绝对值来更新答案
+        // auto update = [&](int cur) {
+        //     if (abs(cur - target) < abs(best - target)) {
+        //         best = cur;
+        //     }
+        // };
+
