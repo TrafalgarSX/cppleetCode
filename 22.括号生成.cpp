2512ab42f34bbad679ca5d349d7c760d1740c0bb
Trafalgar <1148636359g@gmail.com>
2023-05-23 23:54:25 +0800

    回溯

---
+++ 22.括号生成.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=cpp
+ *
+ * [22] 括号生成
+ */
+
+// @lc code=start
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string quote;
+
+        backtrack(ans, quote, n, n);
+        return ans;
+    }
+    void backtrack(vector<string> &ans, string quote, int left, int right){
+        if(left == 0 && right == 0){
+            ans.push_back(quote);
+            return;
+        }
+        if(left == right){
+            backtrack(ans, quote + "(", left - 1, right);
+        }else if(left < right){
+            if(left > 0){
+                backtrack(ans, quote + "(", left - 1, right);
+            }
+            backtrack(ans, quote + ")", left, right - 1);
+        }
+    }
+};
+// @lc code=end
+
