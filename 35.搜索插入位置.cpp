6f6c0737d04528fefb248db2f28a058ada23a30f
Trafalgar <1148636359g@gmail.com>
2023-06-01 00:15:16 +0800

    转化问题：查找第一个大于target的下标。

--- 35.搜索插入位置.cpp
+++ 35.搜索插入位置.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
+    // 在一个有序数组中找第一个大于等于 target 的下标
     int searchInsert(vector<int>& nums, int target) {
         int left = 0, right = nums.size() - 1;
         int ans = nums.size();
@@ -15,12 +16,13 @@ public:
         while(left <= right){
             int mid = left + (right - left) / 2;
             if(nums[mid] == target){
-                ans = mid;
-                right = mid - 1;
+                return mid;
             }else if(nums[mid] < target){
                 left = mid + 1;
             }else if(nums[mid] > target){
+                // 记录当前大于 target的下标
                 ans = mid;
+                // 继续寻找下一个 大于target的下标
                 right = mid - 1;
             }
         }
