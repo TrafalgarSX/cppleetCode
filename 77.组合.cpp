a30ecb43456b4cd20fa1970c0be3f2ed44dcfd03
Trafalgar <1148636359g@gmail.com>
2023-06-04 02:43:21 +0800

    简化上一版方法，很多数组并不需要。

--- 77.组合.cpp
+++ 77.组合.cpp
@@ -12,38 +12,25 @@ using namespace std;
 class Solution {
 public:
   vector<vector<int>> ans;
-  vector<bool> vis;
   vector<vector<int>> combine(int n, int k) {
     vector<int> combine;
 
-    vis.resize(n);
-    vector<int> nums;
-    for (size_t i = 1; i <= n; i++)
-    {
-        nums.push_back(i);
-    }
-    
-    backtrack(combine, nums, 0, 0, k);
+    backtrack(combine, n, 0, 0, k);
 
     return ans;
     }
 
     // 这一层用过的以后都不能用，但下一层用过的可以用
-    void backtrack(vector<int> &combine, vector<int> &nums, int start, int current_depth, int depth){
+    void backtrack(vector<int> &combine, int n, int start, int current_depth, int depth){
         if(current_depth == depth){
             ans.push_back(combine);
             return;
         }
 
-        for (size_t i = start; i < nums.size(); i++)
+        for (size_t i = start; i <  n; i++)
         {
-            if(vis[i])
-                continue;
-            
-            combine.push_back(nums[i]);
-            vis[i] = true;
-            backtrack(combine, nums, i, current_depth + 1, depth);
-            vis[i] = false;
+            combine.push_back(i + 1);
+            backtrack(combine, n, i + 1, current_depth + 1, depth);
             combine.pop_back();
 
         }
