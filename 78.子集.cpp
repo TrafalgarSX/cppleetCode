39e2ceff75719afccaf69ab30ff838b2c1bdebb1
Trafalgar <1148636359g@gmail.com>
2023-06-04 19:00:17 +0800

    新增官方第一种迭代解法， 很直观，很好理解。代码的写法，解法的实现还是要学习，要思考。

--- 78.子集.cpp
+++ 78.子集.cpp
@@ -7,32 +7,53 @@
 #include <vector>
 using namespace std;
 // @lc code=start
+#define MASK 
 class Solution {
 public:
     vector<vector<int>> ans;
     vector<vector<int>> subsets(vector<int>& nums) {
-
+#   ifdef MASK
         vector<int> set;
-        ans.push_back(set);
+        int size = nums.size();
+        for (size_t mask = 0; mask < (1 << size); mask++)
+        {
+            set.clear();
+            for (size_t i = 0; i < size; i++)
+            {
+                if(mask  & (1 << i)){
+                    set.push_back(nums[i]);
+                }
+            }
+            ans.push_back(set);
+        }
+#   else
+        vector<int> set;
         dfs(nums, set, 0, nums.size());
-
+#   endif // MASK
         return ans;
     }
 
     // 77题组合中的递归法实现子集枚举的方式
     void dfs(vector<int> &nums, vector<int> &set, int current_depth, int depth){
         if(current_depth == depth){
-            return;
+          ans.push_back(set);
+          return;
         }
 
+        // 选中当前
         set.push_back(nums[current_depth]);
-        ans.push_back(set);
         dfs(nums, set, current_depth + 1, depth);
-
+        // 不选中档当前
         set.pop_back();
         dfs(nums, set, current_depth + 1, depth);
-
     }
+
+    // 迭代法
+    // 解释： 数组大小为n, 原序列中每个元素的状态有两种： 1-在子集中， 0-不在子集中
+    // 这样每个子集可以用一个长度为 n 的二进制序列表示 mask
+
+    
+
 };
 // @lc code=end
 
