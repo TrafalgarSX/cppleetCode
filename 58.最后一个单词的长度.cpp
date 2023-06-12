3862b629cb9ebc84e56730f43b9bb788080ca56a
Trafalgar <1148636359g@gmail.com>
2023-06-12 23:48:23 +0800

    很简单的题。

---
+++ 58.最后一个单词的长度.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=cpp
+ *
+ * [58] 最后一个单词的长度
+ */
+
+#include <vector>
+#include <string>
+#include <string.h>
+#include <regex>
+#include <sstream>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+
+        // length = lengthOfLastWord_sstream(s);
+        length = lengthOfLastWord_split(s);
+        // length = lengthOfLastWord_regex(s);
+        return length;
+    }
+
+    // regex  too slow
+    int lengthOfLastWord_regex(string &s){
+        std::regex ws_re("\\s+"); // whitespace
+        std::vector<std::string> v(
+            std::sregex_token_iterator(s.begin(), s.end(), ws_re, -1),
+            std::sregex_token_iterator());
+
+        return v.back().length();
+
+    }
+
+    int lengthOfLastWord_split(string &s){
+        char *token = strtok(s.data(), " ");
+        int length;
+        while(token != NULL){
+            length = strlen(token);
+            token = strtok(NULL, " ");
+        }
+        return length;
+    }
+
+    int lengthOfLastWord_sstream(string &s){
+
+        stringstream ss(s);
+        string word;
+        int length;
+
+        while(ss >> word){
+            length = word.length();
+        }
+
+        return length;
+    }
+};
+// @lc code=end
+
