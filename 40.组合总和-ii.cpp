64bf9490e5a9e60c125b8714d07e157929629283
Trafalgar <1148636359g@gmail.com>
2023-06-17 02:06:37 +0800

    比上一题多了一个排序和针对此题的去重， pos + 1保证每个数字在每个组合中只使用一次。

---
+++ 40.组合总和-ii.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=40 lang=cpp
+ *
+ * [40] 组合总和 II
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> combine;
+
+        sort(candidates.begin(), candidates.end());
+        backtrack(candidates, combine, target, 0, 0, 0, candidates.size());
+
+        return ans;
+    }
+
+    void backtrack(vector<int>& candidates, vector<int>& combine, int target, int pos, 
+                                            int sum, int current_depth, int depth){
+        if(sum == target){
+            ans.push_back(combine);
+            return;
+        }
+
+        if(current_depth == depth){
+            return;
+        }
+
+        for (size_t i = pos; i < depth; i++)
+        {
+            // 去重， 这样解集中不会包含重复的组合
+            if( i > pos && candidates[i] == candidates [i - 1]){
+                continue;
+            }
+            int num = candidates.at(i);
+            sum += num;
+            if( sum <= target){
+                combine.push_back(num);
+                backtrack(candidates, combine, target, i + 1, sum,
+                          current_depth + 1, depth);
+                combine.pop_back();
+            }else{
+                break;
+            }
+            sum -= num;
+        }
+
+    }
+};
+// @lc code=end
+int main(int argc, char const *argv[])
+{
+    Solution solution;
+    // solution.combinationSum2();
+    return 0;
+}
+
+
